ab66b595c01ed9d25f5f6a5c282d446c1997b575
abinshoby <abinshoby@gmail.com>
2018-03-06 01:03:01 +0530

    "graph view enabled zooming need to be fixed and also the large data"

--- app/src/main/java/com/tkmce/railwayanalyzer/MainActivity.java
+++ app/src/main/java/com/tkmce/railwayanalyzer/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -21,11 +26,14 @@ public class MainActivity extends AppCompatActivity {
     private static final String pass = "5TDF523yLU7oLJEX";
     TextView tv;
     Connection con = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.texttrail);
+
+
         new connTask().execute();
     }
 
@@ -34,17 +42,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class connTask extends AsyncTask<Void,Void,Void>{
+        private GraphView mGraph;
+        private LineGraphSeries<DataPoint> series;
         int rowCount=0;
+        String time[];
+        Float speed[];
+        Float distance[];
+        Float driver[];
+        int count;
+
         @Override
         protected Void doInBackground(Void... voids) {
+            mGraph =(GraphView)findViewById(R.id.all);
+
+            int size = 0;
             try {
                 Class.forName("com.mysql.jdbc.Driver");
                 con = DriverManager.getConnection(url,user,pass);
                 Statement stmt = con.createStatement();
 
-                ResultSet rs = stmt.executeQuery("Select count(*) From data ;");
-                rs.next();
-                rowCount = rs.getInt(1);
+                ResultSet rs1 = stmt.executeQuery("Select count(*) From data ;");
+                rs1.next();
+                rowCount = rs1.getInt(1);
+                ResultSet  rs=stmt.executeQuery("select  * from data limit 30;");
+
+
+                time = new String[30];
+                speed = new Float[30];
+                distance = new Float[30];
+                driver = new Float[30];//rs.getrow()
+
+
+
+               // viewport.setYAxisBoundsManual(true);
+               // viewport.setMinY(0);
+               // viewport.setMaxY(3000);
+
+
+               rs.first();
+ count=0;
+                while (!rs.isAfterLast() ) {
+            	/* Store Values */
+                    //time[count] = rs.getString("time");
+                    speed[count] = Float.valueOf(rs.getString("speed"));
+
+
+                    distance[count] = Float.valueOf(rs.getString("distance"));
+                    //series.appendData(new DataPoint(speed[count], distance[count]), true, 30000);
+                    driver[count] = Float.valueOf(rs.getString("driver_id"));
+                    rs.next();
+            	/* Increment count */
+                    count++;
+                    //System.out.println(String.valueOf(rs.getString("time")));
+                }
             } catch (ClassNotFoundException e) {
                 Toast.makeText(MainActivity.this, "Class not found", Toast.LENGTH_SHORT).show();
             } catch (SQLException e) {
@@ -57,6 +107,28 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             tv.setText("Success : "+ rowCount + "  rows ");
+            series = new LineGraphSeries<DataPoint>();
+            mGraph.addSeries(series);
+            Viewport viewport = mGraph.getViewport();
+            mGraph.getViewport().setScrollable(true); // enables horizontal scrolling
+            mGraph.getViewport().setScrollableY(true);
+            viewport.setYAxisBoundsManual(true);
+            viewport.setXAxisBoundsManual(true);
+          //viewport.setScalable(true);
+            viewport.setMinY(30);
+            viewport.setMaxY(50);
+            viewport.setMaxX(150);
+            viewport.setMinX(100);
+          //  mGraph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+          //  mGraph.getViewport().setScalableY(true);
+
+           // viewport.setScrollable(true);
+           // viewport.setScrollableY(true);
+            for(int i=0;i<30;i++){
+                series.appendData(new DataPoint(speed[i], distance[i]), true, 30000);
+            }
+
+
         }
     }
 
